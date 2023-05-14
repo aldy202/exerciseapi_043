0ee1bd79d561e608e5e181fac11ad7b0fcff0a29
aldy202 <aldyfauzi202@.gmail.com>
2023-05-14 23:19:20 +0700

    add method delete

--- lib/controller/kategori_barang_controller.dart
+++ lib/controller/kategori_barang_controller.dart
@@ -2,39 +2,48 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:materiapi_2/model/kategori_barang_model.dart';
 
-class BarangController{
-
+class BarangController {
   final String apiUrl = "http://10.0.2.2:8000/api/";
 
-  Future<List<KategoriBarangModel>> getKategoriBarang() async{
+  Future<List<KategoriBarangModel>> getKategoriBarang() async {
     var result = await http.get(Uri.parse("${apiUrl}barang/getAllKB"));
 
-    if(result.statusCode ==  200){
+    if (result.statusCode == 200) {
       var data = json.decode(result.body);
       List<KategoriBarangModel> kategoriBarang = [];
-      for (var i in data){
+      for (var i in data) {
         KategoriBarangModel kategori = KategoriBarangModel.fromMap(i);
         kategoriBarang.add(kategori);
       }
 
       return kategoriBarang;
-    }else{
+    } else {
       throw Exception('Gagal mengambil data kategori barang');
     }
   }
 
-  Future addkategoriBarang(KategoriBarangModel kategoriBarang) async{
-    var result = await http.post(Uri.parse("${apiUrl}barang/addKB/"),
-    body: {
+  Future addkategoriBarang(KategoriBarangModel kategoriBarang) async {
+    var result = await http.post(Uri.parse("${apiUrl}barang/addKB/"), body: {
       "nama_kategori_barang": kategoriBarang.nama,
-    }
-    );
-    if(result.statusCode == 200){
+    });
+    if (result.statusCode == 200) {
       return jsonDecode(result.body);
-    }else{
+    } else {
       throw Exception('Gagal menambahkan data kategori barang');
     }
-
   }
 
+  Future deleteKategoriBarang(int id) async {
+    try {
+      var result = await http.post(Uri.parse("${apiUrl}barang/deleteKB/$id"));
+      if (result.statusCode == 200) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      print(e.toString());
+      throw Exception("gagal hapus data kategori barang");
+    }
+  }
 }
